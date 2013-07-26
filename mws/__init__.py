cf61d7764952e4b80b4eb103c286dc849ff94bf8
Kevin Jones <git@kcjones.org>
2013-07-26 04:06:36 +0000

    made 'from mws import Class' work

--- mws/__init__.py
+++ mws/__init__.py
@@ -1,1 +1,11 @@
+from mws import MWS
+from mws import Feeds
+from mws import Reports
+from mws import Orders
+from mws import Products
+from mws import Sellers
+from mws import InboundShipments
+from mws import Inventory
+from mws import OutboundShipments
+from mws import Recommendations
  
